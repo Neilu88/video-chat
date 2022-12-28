4d9129e5981aaa70a974cb74e4b7fae11925125b
Neil <neild2002@gmail.com>
2022-12-28 12:24:22 -0500

    finalized build

--- prisma/migrations/20221227091010_adding_video_chat_user/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "VideoChatUser" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "contact" TEXT NOT NULL
-);
--- prisma/migrations/20221227143026_adding_match_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Match" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "sourceUserId" TEXT NOT NULL,
-    "endUserId" TEXT NOT NULL,
-    "status" TEXT NOT NULL DEFAULT 'running',
-    CONSTRAINT "Match_sourceUserId_fkey" FOREIGN KEY ("sourceUserId") REFERENCES "VideoChatUser" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "Match_endUserId_fkey" FOREIGN KEY ("endUserId") REFERENCES "VideoChatUser" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
